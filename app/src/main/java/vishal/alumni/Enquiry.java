cac3fad9efd79365049e31f9d3d54a4a04dc2ad1
UV97120 <vishalsheth4@gmail.com>
2016-07-15 17:37:24 +0530

    map activity

--- app/src/main/java/vishal/alumni/Enquiry.java
+++ app/src/main/java/vishal/alumni/Enquiry.java
@@ -2,6 +2,7 @@ package vishal.alumni;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Enquiry extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class Enquiry extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enquiry2);
+        EditText editText1 = (EditText)findViewById(R.id.FirstName);
+
+        EditText editText2 = (EditText)findViewById(R.id.LastName);
+        EditText editText3 = (EditText)findViewById(R.id.userEmailId);
+        EditText editText4 = (EditText)findViewById(R.id.mobileNumber);
+        EditText editText5 = (EditText)findViewById(R.id.multi);
     }
 }
